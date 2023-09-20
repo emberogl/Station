ea4a0fb138740dfa45b3671ff13e06c06d3cd8c2
emberogl <embernebulan@gmail.com>
2023-09-20 13:18:01 +0200

    Add project files.

---
+++ Model/Animal.cs
@@ -0,0 +1,28 @@
+﻿namespace Station.Model
+{
+    internal class Animal
+    {
+        protected private string _name;
+        protected private string _species;
+        protected private string _reproductionType;
+        protected private int _age;
+        protected private int _legCount;
+        protected private string _furType;
+
+        public Animal(string name, string species, string reproductionType, int age, int legCount, string furType)
+        {
+            _name = name;
+            _species = species;
+            _reproductionType = reproductionType;
+            _age = age;
+            _legCount = legCount;
+            _furType = furType;
+        }
+
+        public string IncrementAge(int years)
+        {
+            _age += years;
+            return $"{_name}'s age increased to {_age}";
+        }
+    }
+}
